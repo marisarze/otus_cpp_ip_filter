70dc6d22436fd62d84c1512a6d97f05938c69d4d
marisarze <flypetrov@yandex.ru>
2022-07-26 19:28:09 +0300

    all sorting variants check 3 md5sum

--- main.cpp
+++ main.cpp
@@ -7,18 +7,15 @@ int main(int argc, char const *argv[])
     {
         auto ip_pool = get_ip_pool(std::cin);
         show_ip_pool(ip_pool);
-        std::cout << "---------------------------------" << std::endl;
+        // std::cout << "---------------------------------" << std::endl;
         auto new_pool = reverse_sort(ip_pool);
         show_ip_pool(new_pool);
-        std::cout << "---------------------------------" << std::endl;
         auto condition_func1 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="1";};
         new_pool = sort_by_condition(ip_pool, condition_func1);
         show_ip_pool(new_pool);
-        std::cout << "---------------------------------" << std::endl;
         auto condition_func2 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="46" && (*it)[1]=="70";};
         new_pool = sort_by_condition(ip_pool, condition_func2);
         show_ip_pool(new_pool);
-        std::cout << "---------------------------------" << std::endl;
         auto condition_func3 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {
             for (int i=0;i<4;i++){
                 if ((*it)[i]=="46") return true;
