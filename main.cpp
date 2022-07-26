b7fb3655fecd024f447df9d885a594616aa21979
marisarze <flypetrov@yandex.ru>
2022-07-26 19:22:17 +0300

    all sorting variants check 2

--- main.cpp
+++ main.cpp
@@ -11,21 +11,21 @@ int main(int argc, char const *argv[])
         auto new_pool = reverse_sort(ip_pool);
         show_ip_pool(new_pool);
         std::cout << "---------------------------------" << std::endl;
-        auto condition_func = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="1";};
-        new_pool = sort_by_condition(ip_pool, condition_func);
+        auto condition_func1 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="1";};
+        new_pool = sort_by_condition(ip_pool, condition_func1);
         show_ip_pool(new_pool);
         std::cout << "---------------------------------" << std::endl;
-        auto condition_func = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="46" && (*it)[1]=="70";};
-        new_pool = sort_by_condition(ip_pool, condition_func);
+        auto condition_func2 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="46" && (*it)[1]=="70";};
+        new_pool = sort_by_condition(ip_pool, condition_func2);
         show_ip_pool(new_pool);
         std::cout << "---------------------------------" << std::endl;
-        auto condition_func = [](std::vector<std::vector <std::string>>::iterator it)-> bool {
+        auto condition_func3 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {
             for (int i=0;i<4;i++){
                 if ((*it)[i]=="46") return true;
             }
             return false;
         };
-        new_pool = sort_by_condition(ip_pool, condition_func);
+        new_pool = sort_by_condition(ip_pool, condition_func3);
         show_ip_pool(new_pool);
     }
     catch(const std::exception &e)
