318ef7726a6b49dab4d6880228ef3741cace1e05
marisarze <flypetrov@yandex.ru>
2022-07-26 20:19:51 +0300

    check md5sum 3

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[])
         show_ip_pool(new_pool);
         auto condition_func2 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="46" && (*it)[1]=="70";};
         new_pool = sort_by_condition(new_pool, condition_func2);
-        std::cout << "Size after codition 2: " << new_pool.size() << std::endl;
+        // std::cout << "Size after codition 2: " << new_pool.size() << std::endl;
         //show_ip_pool(new_pool);
         auto condition_func3 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {
             for (int i=0;i<4;i++){
@@ -27,7 +27,7 @@ int main(int argc, char const *argv[])
             return false;
         };
         new_pool = sort_by_condition(new_pool, condition_func3);
-        std::cout << "Size after codition 3: " << new_pool.size() << std::endl;
+        // std::cout << "Size after codition 3: " << new_pool.size() << std::endl;
         //show_ip_pool(new_pool);
     }
     catch(const std::exception &e)
