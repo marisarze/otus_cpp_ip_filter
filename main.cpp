b518a4237415f41de750f289a61d66368a1a5814
marisarze <flypetrov@yandex.ru>
2022-07-26 20:16:24 +0300

    check md5sum

--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ int main(int argc, char const *argv[])
     try
     {
         auto ip_pool = get_ip_pool(std::cin);
-        std::cout << "Size original: " << ip_pool.size() << std::endl;
-        //show_ip_pool(ip_pool);
+        //std::cout << "Size original: " << ip_pool.size() << std::endl;
+        show_ip_pool(ip_pool);
         // std::cout << "---------------------------------" << std::endl;
         auto new_pool = reverse_sort(ip_pool);
-        std::cout << "Size after reverse: " << new_pool.size() << std::endl;
-        //show_ip_pool(new_pool);
+        //std::cout << "Size after reverse: " << new_pool.size() << std::endl;
+        show_ip_pool(new_pool);
         auto condition_func1 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="1";};
-        new_pool = sort_by_condition(ip_pool, condition_func1);
-        std::cout << "Size after codition 1: " << new_pool.size() << std::endl;
-        //show_ip_pool(new_pool);
+        new_pool = sort_by_condition(new_pool, condition_func1);
+        //std::cout << "Size after codition 1: " << new_pool.size() << std::endl;
+        show_ip_pool(new_pool);
         auto condition_func2 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="46" && (*it)[1]=="70";};
-        new_pool = sort_by_condition(ip_pool, condition_func2);
+        new_pool = sort_by_condition(new_pool, condition_func2);
         std::cout << "Size after codition 2: " << new_pool.size() << std::endl;
         //show_ip_pool(new_pool);
         auto condition_func3 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {
@@ -26,7 +26,7 @@ int main(int argc, char const *argv[])
             }
             return false;
         };
-        new_pool = sort_by_condition(ip_pool, condition_func3);
+        new_pool = sort_by_condition(new_pool, condition_func3);
         std::cout << "Size after codition 3: " << new_pool.size() << std::endl;
         //show_ip_pool(new_pool);
     }
