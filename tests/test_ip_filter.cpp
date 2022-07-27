0ac50fdf1a30e193ff292270a1e6a9f32b7a1c8c
marisarze <flypetrov@yandex.ru>
2022-07-27 11:57:01 +0300

    check all tests

--- tests/test_ip_filter.cpp
+++ tests/test_ip_filter.cpp
@@ -29,7 +29,6 @@ TEST(TestSimple, TestGetIpPool){
         {"1", "2", "3", "4"}
     };
     std::cin.rdbuf(backup);
-    show_ip_pool(result);
     EXPECT_EQ(result, expected);
 }
 
@@ -103,4 +102,65 @@ TEST(TestSimple, TestReverseSort){
     };
     auto result = reverse_sort(input_pool);
     EXPECT_EQ(result, expected);
+}
+
+
+TEST(TestSimple, TestFilterByCondition){
+
+    std::vector <std::vector<std::string>> input_pool = 
+    {
+        {"1", "2", "3", "4"},
+        {"2", "3", "4", "5"},
+        {"3", "4", "5", "6"},
+        {"4", "8", "8", "8"},
+        {"46", "70", "8", "9"},
+        {"6", "5", "5", "4"},
+        {"46", "70", "0"},
+        {"1", "70", "7", "0"},
+        {"6", "5", "5", "3"},
+        {"46", "70", "5", "4"},
+        {"5", "6", "46", "4"},
+        {"240", "250", "6", "46"},
+        {"1", "8", "7", "46"}
+    };
+
+    auto condition_func1 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="1";};
+    auto result_1 = filter_by_condition(input_pool, condition_func1);
+    decltype(input_pool) expected_1 = 
+    {
+        {"1", "2", "3", "4"},
+        {"1", "70", "7", "0"},
+        {"1", "8", "7", "46"}
+    };
+
+    auto condition_func2 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {return (*it)[0]=="46" && (*it)[1]=="70";};
+    auto result_2 = filter_by_condition(input_pool, condition_func2);
+    decltype(input_pool) expected_2 = 
+    {
+        {"46", "70", "8", "9"},
+        {"46", "70", "0"},
+        {"46", "70", "5", "4"}
+    };
+
+
+    auto condition_func3 = [](std::vector<std::vector <std::string>>::iterator it)-> bool {
+        for (int i=0;i<4;i++){
+            if ((*it)[i]=="46") return true;
+        }
+        return false;
+    };
+    auto result_3 = filter_by_condition(input_pool, condition_func3);
+    decltype(input_pool) expected_3 = 
+    {   
+        {"46", "70", "8", "9"},
+        {"46", "70", "0"},
+        {"46", "70", "5", "4"},
+        {"5", "6", "46", "4"},
+        {"240", "250", "6", "46"},
+        {"1", "8", "7", "46"}
+    };
+
+    EXPECT_EQ(result_1, expected_1);
+    EXPECT_EQ(result_2, expected_2);
+    EXPECT_EQ(result_3, expected_3);
 }
