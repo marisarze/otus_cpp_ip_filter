1546fd86c58363ba3ff62ba38c87d72aa068459c
marisarze <flypetrov@yandex.ru>
2022-09-11 21:41:56 +0300

    change main

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@ int main()
         const std::vector <uint8_t> some_val;
 
         auto condition_func1 = [](decltype(some_val)& input)-> bool {return input[0]==1;};
-        new_pool = filter_by_condition(new_pool, condition_func1);
-        show_ip_pool(new_pool);
+        auto filtered_pool = filter_by_condition(new_pool, condition_func1);
+        show_ip_pool(filtered_pool);
 
         auto condition_func2 = [](decltype(some_val)& input)-> bool {return input[0]==46 && input[1]==70;};
-        new_pool = filter_by_condition(new_pool, condition_func2);
-        show_ip_pool(new_pool);
+        filtered_pool = filter_by_condition(new_pool, condition_func2);
+        show_ip_pool(filtered_pool);
 
         auto condition_func3 = [](decltype(some_val)& input)-> bool {
             for (int i=0;i<4;i++){
@@ -26,8 +26,8 @@ int main()
             }
             return false;
         };
-        new_pool = filter_by_condition(new_pool, condition_func3);
-        show_ip_pool(new_pool);
+        filtered_pool = filter_by_condition(new_pool, condition_func3);
+        show_ip_pool(filtered_pool);
         
     }
     catch(const std::exception &e)
